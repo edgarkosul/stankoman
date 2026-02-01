9c454dda8a70eeeb4a69d86101079eff3399732f
Edgar Kosulin <kosulin.siteko@gmail.com>
2026-02-01 22:02:37 +0300

    Initial commit

---
+++ routes/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::livewire('settings/profile', Profile::class)->name('profile.edit');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::livewire('settings/password', Password::class)->name('user-password.edit');
+    Route::livewire('settings/appearance', Appearance::class)->name('appearance.edit');
+});
