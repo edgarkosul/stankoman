5b08772dd9523194dbe6bdcd82f3ae808be97dce
Edgar Kosulin <kosulin.siteko@gmail.com>
2026-02-07 20:06:13 +0300

    Home page padding tunning

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
             $content = blank($homePage?->content) ? '<p></p>' : $homePage->content;
         @endphp
 
-        <div class="fi-prose bg-zinc-50 max-w-7xl mx-auto px-6">
+        <div class="fi-prose bg-zinc-50 max-w-7xl mx-auto px-2 xs:px-3 sm:px-4 md:px-6">
             {!! Filament\Forms\Components\RichEditor\RichContentRenderer::make($content)->plugins([
                     App\Filament\Forms\Components\RichEditor\Plugins\TextSizeRichContentPlugin::make(),
                 ])->customBlocks([
